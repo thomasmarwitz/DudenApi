41d15195cf8efb02ad117552d0beebd38886f79f
Thomas Marwitz <thomas.marwitz@gmx.de>
2020-05-24 13:22:08 +0200

    found tag to inspect -> element.html further investigation needed

--- api.py
+++ api.py
@@ -5,6 +5,6 @@ response = requests.get("https://www.duden.de/rechtschreibung/Haus")
 
 soup = BeautifulSoup(response.text, "lxml", multi_valued_attributes=None)
 
-for tag in soup.find_all("h2"):
-    if tag["class"] == "division__title":
-        print(tag.get_text())
+h2 = [tag for tag in soup.find_all("h2") if tag["class"] == "division__title"]
+# Bedeutung: contains text
+tag = h2[1].parent.parent #h2[i]
