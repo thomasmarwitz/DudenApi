64e660d77b84033e020219579bac8cc1a97aec26
Thomas Marwitz <thomas.marwitz@gmx.de>
2020-05-25 18:15:33 +0200

    Targeting "division " tag

--- api.py
+++ api.py
@@ -5,6 +5,21 @@ response = requests.get("https://www.duden.de/rechtschreibung/Haus")
 
 soup = BeautifulSoup(response.text, "lxml", multi_valued_attributes=None)
 
-h2 = [tag for tag in soup.find_all("h2") if tag["class"] == "division__title"]
-# Bedeutung: contains text
-tag = h2[1].parent.parent #h2[i]
+for tag in soup.find_all("div", multi_valued_attributes=None):
+    try:
+        if tag["class"] == "division ":
+            print(tag["id"])
+    except KeyError:
+        pass
+# yields
+"""
+rechtschreibung
+bedeutungen
+synonyme
+herkunft
+grammatik
+wussten_sie_schon
+aussprache
+kontext
+block-beforeafterblock-2
+"""
